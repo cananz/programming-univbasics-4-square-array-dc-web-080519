ab88530980fd9a6adba92d20c539015486d5f58c
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-07 21:50:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,3 +8,13 @@ def square_array(array)
    counter += 1
  end
 end
+
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
+end
